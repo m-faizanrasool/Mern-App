1e25c55cebfe9a03e70796e7cfe0814347ac3bde
Faizan <htc.erox@gmail.com>
2019-03-23 22:55:17 +0500

    deploying heroku try2

--- server.js
+++ server.js
@@ -1,58 +1,66 @@
 
-// var express = require('express');
-// var bodyParser = require("body-parser");
-
-// var users = [
-//     { id: 1, username: "umar", password: 'abcd1234' },
-//     { id: 2, username: "test", password: '1234' },
-// ]
-
-// var server = express()
-
-// server.use(express.static('./build'))
-// server.use(bodyParser.urlencoded({ extended: true }))
-// server.use(bodyParser.json())
-
-// require('./server/config/db-config');
-// require('./server/routes/all-routes')(server);
-
-// server.use((err, req, res, next) => {
-//     console.warn(err)
-//     res.status(500).send("Error Catched by error handler.")
-// })
-
-// server.listen(process.env.PORT || 5000, () => console.log("server is running"))
-
-
-
-
-
-
-
-
-const express = require('express');
-
-const app = express();
+var express = require('express');
+var bodyParser = require("body-parser");
 const path = require('path');
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, name: 'Bed', price: '50000'},
-    {id: 2, name: 'Sofa', price: '40000'},
-    {id: 3, name: 'Chair', price: '5000'},
-  ];
 
-  res.json(customers);
-});
+var users = [
+    { id: 1, username: "umar", password: 'abcd1234' },
+    { id: 2, username: "test", password: '1234' },
+]
 
-const port = process.env.PORT || 5000; //step 1
+var server = express()
 
+server.use(express.static('./build'))
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+require('./server/config/db-config');
+require('./server/routes/all-routes')(server);
+
+server.use((err, req, res, next) => {
+    console.warn(err)
+    res.status(500).send("Error Catched by error handler.")
+})
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( 'client/build' ));
+  server.use(express.static( 'client/build' ));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__firname, 'client', 'build', 'index.html'));
-    })
+  server.get('*', (req, res) => {
+      res.sendFile(path.join(__firname, 'client', 'build', 'index.html'));
+  })
 }
+server.listen(process.env.PORT || 5000, () => console.log("server is running"))
 
-app.listen(port, () => `Server running on port ${port}`);
+
+
+
+
+
+
+
+// const express = require('express');
+
+// const app = express();
+// const path = require('path');
+
+// app.get('/api/customers', (req, res) => {
+//   const customers = [
+//     {id: 1, name: 'Bed', price: '50000'},
+//     {id: 2, name: 'Sofa', price: '40000'},
+//     {id: 3, name: 'Chair', price: '5000'},
+//   ];
+
+//   res.json(customers);
+// });
+
+// const port = process.env.PORT || 5000; //step 1
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static( 'client/build' ));
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__firname, 'client', 'build', 'index.html'));
+//     })
+// }
+
+// app.listen(port, () => `Server running on port ${port}`);
